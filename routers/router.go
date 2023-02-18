e0a5797e4428fd7e292a1f2f422d549ed667834a
zhaogongchengsi <zzh1586169624@163.com>
2023-02-18 15:59:53 +0800

    change

--- routers/router.go
+++ routers/router.go
@@ -4,10 +4,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CreateAppRouter () *gin.Engine  {
+func CreateAppRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.GET("/hello", hello)
 	return r
 }
 
+func hello(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello starter Gin",
+	})
+}
