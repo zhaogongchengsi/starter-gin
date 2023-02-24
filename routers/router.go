2ee40442a80c8bff06872b9fc212e796a7782630
zhaogongchengsi <zzh1586169624@163.com>
2023-02-24 16:21:27 +0800

    change

--- routers/router.go
+++ routers/router.go
@@ -1,14 +1,28 @@
 package routers
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/server-gin/global"
 )
 
 func CreateAppRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
-	r.GET("/", hello)
+
+	r.Static("/static", global.ServerConfig.Static)
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.Redirect(http.StatusFound, global.ServerConfig.Static+"/index.html")
+	})
+
+	v1 := r.Group(fmt.Sprintf("/%s", global.ServerConfig.Prefix))
+
+	v1.GET("hello", hello)
+
 	return r
 }
 
