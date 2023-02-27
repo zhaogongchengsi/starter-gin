97238321ce782de735cf9d8cbe15a78ba7922f6e
zhaogongchengsi <zzh1586169624@163.com>
2023-02-27 10:49:58 +0800

    feat: Proxy for static files

--- routers/router.go
+++ routers/router.go
@@ -11,11 +11,7 @@ func CreateAppRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
-
-	r.Static(global.ServerConfig.StaticName, global.ServerConfig.Static)
-	r.StaticFile("/", global.ServerConfig.IndexHtml)
-
-	// r.Static("/favicon.ico", "./dist/favicon.ico")
+	CreateStaticRouter(r)
 
 	v1 := r.Group(fmt.Sprintf("/%s", global.ServerConfig.Prefix))
 
@@ -28,4 +24,9 @@ func hello(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "Hello starter Gin",
 	})
+}
+
+func CreateStaticRouter(r *gin.Engine) {
+	r.Static("/assets", "./static/assets")
+	r.StaticFile("/", "./static/index.html")
 }
