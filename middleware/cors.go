3fef1ee422c7bb6f138b2d0d3646af3f6d356406
zhaogongchengsi <zzh1586169624@163.com>
2023-02-28 16:43:00 +0800

    feat: cross domain

---
+++ middleware/cors.go
@@ -0,0 +1,45 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CoreOption struct {
+	AllowHeaders string
+	AllowMethods string
+	AllCred      string
+	ExpHeaders   string
+}
+
+var defaultOpt = &CoreOption{
+	AllowHeaders: "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token",
+	AllowMethods: "POST, GET, OPTIONS,DELETE,PUT",
+	ExpHeaders:   "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type",
+	AllCred:      "true",
+}
+
+func DefaultCoreOption() *CoreOption {
+	return defaultOpt
+}
+
+func Cors(opt CoreOption) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+		origin := c.Request.Header.Get("Origin")
+
+		c.Header("Access-Control-Allow-Origin", origin)
+		c.Header("Access-Control-Allow-Headers", opt.AllowHeaders)
+		c.Header("Access-Control-Allow-Methods", opt.AllowMethods)
+		c.Header("Access-Control-Expose-Headers", opt.ExpHeaders)
+		c.Header("Access-Control-Allow-Credentials", opt.AllCred)
+
+		// 放行所有OPTIONS方法
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		// 处理请求
+		c.Next()
+	}
+}
